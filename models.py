40106a549020ac6ce8476e6610a12dc9915c1c65
salfaris <sal.ahmadfaris@gmail.com>
2020-11-05 11:27:52 +0000

    UPDATE: Added error checking for generate_table.

--- models.py
+++ models.py
@@ -40,11 +40,19 @@ class IntegralKernel:
         generate_table(table_header, col_method, col_return)
         return self
     
+class NotMatchError(Exception):
+    pass
+
 def generate_table(header: List[str], *columns: List[Any]):
     """Code credit: https://stackoverflow.com/a/39032993/13698939"""
     
+    if len(header) != len(columns):
+        msg = "Expect column header to match number of columns."
+        raise NotMatchError(msg)
+    
     table_data = [header] + list(zip(*columns))
     
+    # Begin printing the table
     for idx, row in enumerate(table_data):
         line = '|'.join(str(x).ljust(12) for x in row)
         print(line)
