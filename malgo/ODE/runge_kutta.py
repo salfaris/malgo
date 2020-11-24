7996bcf3cd6b7b9752115f13633e310da63b5a80
salfaris <sal.ahmadfaris@gmail.com>
2020-11-24 15:29:00 +0000

    NEW: Added fourth-order Runge-Kutta

--- malgo/ODE/runge_kutta.py
+++ malgo/ODE/runge_kutta.py
@@ -5,7 +5,32 @@ import numpy as np
 from typing import List
 from malgo.custom_type import Function, Real, Vector
 
-def runge_kutta(init_conds: List[Real], a: Real, b: Real, N: int, f: Function):
+def runge_kutta_4th(init_conds: List[Real], a: Real, b: Real, N: int, f: Function):
+    """Solves an ODE using the fourth-order Runge-Kutta method.
+    
+    Args:
+       init_conds: List of initial value conditions.
+       a: Initial value of x, also used as startpoint of interval.
+       b: Endpoint of interval.
+       N: Number of subintervals.
+       f: The n-1 th order system if this is an n-th 
+            order ODE. It admits the form f(x, y_vec).
+    """
+    h = float(b-a) / N
+    ys = np.zeros((N+1, len(init_conds)))
+    ys[0] = init_conds
+    xs = np.arange(start=a, stop=b+h, step=h)
+    
+    for i in range(N):
+        xi, yi = a+i*h, ys[i]
+        K0 = h * F_vec(xi, yi, f)
+        K1 = h * F_vec(xi+h/2, yi+K0/2, f)
+        K2 = h * F_vec(xi+h/2, yi+K1/2, f)
+        K3 = h * F_vec(xi+h, yi+K2, f)
+        ys[i+1] = yi + 1./6*(K0 + 2*K1 + 2*K2 + K3)
+    return xs, ys
+
+def runge_kutta_2nd(init_conds: List[Real], a: Real, b: Real, N: int, f: Function):
     """Solves an ODE using the second-order Runge-Kutta method.
     
     Args:
@@ -52,5 +77,7 @@ if __name__ == '__main__':
         return x
 
     init_conds = [1.]
-    xs, ys = runge_kutta(init_conds, a=0, b=1, N=1000, f=func)
-    print(ys)
+    xs1, ys1 = runge_kutta_2nd(init_conds, a=0, b=1, N=1000, f=func)
+    xs2, ys2 = runge_kutta_4th(init_conds, a=0, b=1, N=1000, f=func)
+    print(ys1)
+    print(ys2)
